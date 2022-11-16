aafd9e4a860481307c4e6be1bf65739d00e359f0
younus <younusmamunu@gmail.com>
2022-11-16 07:44:56 +0600

    episode 5

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Models\Post;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/greetings', function () {
+//     return "Hello world";
+// });
+
+Route::get('/', function () {
+    return view('welcome');
+    //return "Hello world";
+    //return ['foo'=>'bar'];  //json
+});
+
